bc32b4e54d3fb9488ba67a65216587625ec8a8d1
Nuno Fachada <nuno.fachada@ulusofona.pt>
2018-03-18 17:36:37 +0000

    Add some comments and simplify predicate function for finding specific nodes in lists of node records

--- PathFinding/Graph.cs
+++ PathFinding/Graph.cs
@@ -5,9 +5,13 @@ namespace PathFinding
 {
     public class Graph : IGraph
     {
-
+        // The graph is internally represented using an adjacency list
         private IEnumerable<IConnection>[] connections;
 
+        /// <summary>
+        /// This private class is used to keep node records for the path
+        /// finding algorithms.
+        /// </summary>
         private class NodeRecord : IComparable<NodeRecord>
         {
             public int Node { get; }
@@ -27,56 +31,90 @@ namespace PathFinding
             }
         }
 
+        /// <summary>
+        /// Number of nodes in this graph.
+        /// </summary>
         public int NumberOfNodes { get => connections.Length; }
 
+        /// <summary>
+        /// Create new graph using an adjacency list.
+        /// </summary>
+        /// <param name="connections">Adjacency list with which to create
+        /// graph.</param>
         public Graph(IEnumerable<IConnection>[] connections)
         {
             this.connections = connections;
         }
 
+        /// <summary>
+        /// Create new graph using an adjacency matrix.
+        /// </summary>
+        /// <param name="adjMatrix">Adjacency matrix with which to create
+        /// graph.</param>
         public Graph(float[,] adjMatrix)
         {
+            // Adjacency matrix must be square
             if (adjMatrix.GetLength(0) != adjMatrix.GetLength(1))
             {
                 throw new Exception("Adjacency matrix must be square!");
             }
 
+            // Initialize the internal adjacency list which defines this graph
             connections = new IEnumerable<IConnection>[adjMatrix.GetLength(0)];
 
+            // Cycle through the adjaceny matrix and build the internal
+            // adjacency list
             for (int i = 0; i < adjMatrix.GetLength(0); i++)
             {
+                // Create list for current node
                 List<Connection> currList = new List<Connection>();
 
+                // Populate list for current node
                 for (int j = 0; j < adjMatrix.GetLength(1); j++)
                 {
+                    // Only add connections if weight/cost is larger than 0
                     if (adjMatrix[i, j] > 0.0f)
                     {
                         currList.Add(new Connection(adjMatrix[i, j], i, j));
                     }
                 }
 
+                // Keep current node's list in the adjacency list
                 connections[i] = currList;
-
             }
         }
 
+        /// <summary>
+        /// Get all outgoing connections for the given node.
+        /// </summary>
+        /// <param name="fromNode">A node in the graph.</param>
+        /// <returns>All outgoing connections for the given node.</returns>
         public IEnumerable<IConnection> GetConnections(int fromNode)
         {
             return connections[fromNode];
         }
 
+        /// <summary>
+        /// Find shortest path between start and goal nodes.
+        /// </summary>
+        /// <param name="start">Start node.</param>
+        /// <param name="goal">Goal node.</param>
+        /// <returns>
+        /// An enumerable containing the connections that constitute
+        /// the shortest path from start to goal.
+        /// </returns>
         public IEnumerable<IConnection> PathfindDijkstra(int start, int goal)
         {
 
-            int current; 
+            int current;
             List<NodeRecord> open, closed;
             NodeRecord[] nodeRecords = new NodeRecord[NumberOfNodes];
 
             // Initialize the record for the start node
             nodeRecords[start] = new NodeRecord(start);
 
-            // There is no "current" node, set it to -1
-            current = start; 
+            // "Current" node is start node
+            current = start;
 
             // Initialize the open and closed lists
             open = new List<NodeRecord>() { nodeRecords[start] };
@@ -86,7 +124,7 @@ namespace PathFinding
             while (open.Count > 0)
             {
 
-                // Find the smallest element in the open list
+                // Find element with smallest cost so far in the open list
                 open.Sort();
                 current = open[0].Node;
 
@@ -103,21 +141,19 @@ namespace PathFinding
                     NodeRecord nodeRec;
 
                     // Function to find specific node in a list
-                    Func<NodeRecord, bool> findNode = 
-                        nr => nr.Node == conn.ToNode;
-                    Predicate<NodeRecord> fnp = 
-                        new Predicate<NodeRecord>(findNode);
+                    Predicate<NodeRecord> findNodePred =
+                        new Predicate<NodeRecord>(nr => nr.Node == conn.ToNode);
 
                     // Get cost estimate for the "to node"
                     float toNodeCost =
                         nodeRecords[current].CostSoFar + conn.Cost;
 
                     // Skip if the node is closed
-                    nrIndex = closed.FindIndex(fnp);
+                    nrIndex = closed.FindIndex(findNodePred);
                     if (nrIndex >= 0) continue;
 
                     // If node is open...
-                    nrIndex = open.FindIndex(fnp);
+                    nrIndex = open.FindIndex(findNodePred);
                     if (nrIndex >= 0)
                     {
                         // ...and we find a worse route, also skip
