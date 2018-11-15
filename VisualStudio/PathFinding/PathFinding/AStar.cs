b6fd5ad850acb5e4865a0b8308fa72a1467a7e4d
Nuno Fachada <nuno.fachada@ulusofona.pt>
2018-11-15 20:18:41 +0000

    Add A* implementation and tests

--- VisualStudio/PathFinding/PathFinding/AStar.cs
+++ VisualStudio/PathFinding/PathFinding/AStar.cs
@@ -3,44 +3,48 @@ using System.Collections.Generic;
 
 namespace PathFinding
 {
-    public static class Dijkstra
+    public static class AStar
     {
 
         /// <summary>
-        /// This private class is used to keep node records for the shortest
-        /// path algorithm.
+        /// This private class is used to keep node records for the path
+        /// finding algorithm.
         /// </summary>
         private class NodeRecord : IComparable<NodeRecord>
         {
             public int Node { get; }
             public IConnection Connection { get; set; }
             public float CostSoFar { get; set; }
+            public float EstimatedTotalCost { get; set; }
 
             public NodeRecord(int node)
             {
                 Node = node;
                 Connection = null;
                 CostSoFar = 0.0f;
+                EstimatedTotalCost = 0.0f;
             }
 
             public int CompareTo(NodeRecord other)
             {
-                return Math.Sign(CostSoFar - other.CostSoFar);
+                return Math.Sign(
+                    EstimatedTotalCost - other.EstimatedTotalCost);
             }
         }
 
         /// <summary>
-        /// Find shortest path between start and goal nodes.
+        /// Find a path between start and goal nodes.
         /// </summary>
         /// <param name="graph">Graph where to perform search.</param>
         /// <param name="start">Start node.</param>
         /// <param name="goal">Goal node.</param>
+        /// <param name="heuristics">Heuristics for each node.</param>
         /// <returns>
         /// An enumerable containing the connections that constitute
-        /// the shortest path from start to goal.
+        /// a path from start to goal.
         /// </returns>
-        public static IEnumerable<IConnection> GetShortestPath(
-            IGraph graph, int start, int goal)
+        public static IEnumerable<IConnection> GetPath(
+            IGraph graph, int start, int goal, float[] heuristics)
         {
 
             int current;
@@ -49,6 +53,7 @@ namespace PathFinding
 
             // Initialize the record for the start node
             nodeRecords[start] = new NodeRecord(start);
+            nodeRecords[start].EstimatedTotalCost = heuristics[start];
 
             // "Current" node is start node
             current = start;
@@ -61,7 +66,8 @@ namespace PathFinding
             while (open.Count > 0)
             {
 
-                // Find element with smallest cost so far in the open list
+                // Find element with smallest estimated total cost so far in
+                // the open list
                 open.Sort();
                 current = open[0].Node;
 
@@ -74,9 +80,6 @@ namespace PathFinding
                     // Index of node record in the open and closed lists
                     int nrIndex;
 
-                    // The node record itself
-                    NodeRecord nodeRec;
-
                     // Function to find specific node in a list
                     Predicate<NodeRecord> findNodePred =
                         new Predicate<NodeRecord>(nr => nr.Node == conn.ToNode);
@@ -85,35 +88,39 @@ namespace PathFinding
                     float toNodeCost =
                         nodeRecords[current].CostSoFar + conn.Cost;
 
-                    // Skip if the node is closed
+                    // Check if the node is in the closed list
                     nrIndex = closed.FindIndex(findNodePred);
-                    if (nrIndex >= 0) continue;
-
-                    // If node is open...
-                    nrIndex = open.FindIndex(findNodePred);
                     if (nrIndex >= 0)
                     {
+                        if (nodeRecords[conn.ToNode].CostSoFar <= toNodeCost)
+                            continue;
+                        closed.Remove(nodeRecords[conn.ToNode]);
+                        open.Add(nodeRecords[conn.ToNode]);
+                    }
+                    else if ((nrIndex = open.FindIndex(findNodePred)) >= 0)
+                    {
+                        // If node is in the open list...
                         // ...and we find a worse route, also skip
-                        if (open[nrIndex].CostSoFar <= toNodeCost) continue;
-
-                        // Otherwise, keep node record
-                        nodeRec = open[nrIndex];
+                        if (open[nrIndex].CostSoFar <= toNodeCost)
+                            continue;
                     }
                     else
                     {
                         // If we're here we've got an unvisited node, so make
                         // a record for it
-                        nodeRec = new NodeRecord(conn.ToNode);
-                        nodeRecords[conn.ToNode] = nodeRec;
-
-                        // And add it to the open list
-                        open.Add(nodeRec);
+                        nodeRecords[conn.ToNode] = new NodeRecord(conn.ToNode);
                     }
 
                     // We're here if we need to update the node
                     // Update the cost and connection
-                    nodeRec.CostSoFar = toNodeCost;
-                    nodeRec.Connection = conn;
+                    nodeRecords[conn.ToNode].CostSoFar = toNodeCost;
+                    nodeRecords[conn.ToNode].Connection = conn;
+                    nodeRecords[conn.ToNode].EstimatedTotalCost =
+                        toNodeCost + heuristics[conn.ToNode];
+
+                    // Add node to the open list if it's not already there
+                    if ((nrIndex = open.FindIndex(findNodePred)) < 0)
+                        open.Add(nodeRecords[conn.ToNode]);
                 }
 
                 // We've finished looking at the connections for the current
