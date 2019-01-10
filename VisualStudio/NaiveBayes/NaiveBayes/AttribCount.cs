aae54f098b16f4118951400ae24b9744261b4033
Nuno Fachada <nuno.fachada@ulusofona.pt>
2019-01-10 00:32:22 +0000

    Naive Bayes: add comments and license to source files

--- VisualStudio/NaiveBayes/NaiveBayes/AttribCount.cs
+++ VisualStudio/NaiveBayes/NaiveBayes/AttribCount.cs
@@ -1,11 +1,21 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Author: Nuno Fachada
+ * */
 using System.Collections.Generic;
 
 namespace NaiveBayes
 {
+    // Represents counts for the individual attribute values
+    // This class is for internal consumption only
     internal class AttribCount
     {
+        // Contains the counts for each attribute value
         private IDictionary<string, int> counts;
 
+        // Constructor, initializes all counts to zero
         public AttribCount(Attrib attribute)
         {
             counts = new Dictionary<string, int>();
@@ -15,11 +25,13 @@ namespace NaiveBayes
             }
         }
 
+        // Get count for a given value
         public int GetCount(string value)
         {
             return counts[value];
         }
 
+        // Increment count for a given value
         public void AddCount(string value, int count)
         {
             counts[value] += count;
--- VisualStudio/NaiveBayes/Test/Program.cs
+++ VisualStudio/NaiveBayes/Test/Program.cs
@@ -1,22 +1,34 @@
-﻿using System;
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Author: Nuno Fachada
+ * */
+using System;
 using System.Collections.Generic;
 using NaiveBayes;
 
 namespace Test
 {
+    // Test the Naive Bayes Classifier
     class Program
     {
         static void Main(string[] args)
         {
+            // Create two attributes and specify their possible values
             Attrib distance =
                 new Attrib("distance", new string[] { "near", "far" });
             Attrib speed =
                 new Attrib("speed", new string[] { "slow", "fast" });
 
+            // Create a naive Bayes classifier with a set of labels and a
+            // set of attributes
             NaiveBayesClassifier nbc = new NaiveBayesClassifier(
                 new string[] { "Y", "N" },
                 new Attrib[] { distance, speed });
 
+            // Pass a few observations to the naive Bayes classifier
+
             nbc.Update("Y", new Dictionary<Attrib, string>()
             {
                 { distance, "near"},
@@ -59,12 +71,14 @@ namespace Test
                 { speed, "fast" }
             });
 
+            // Make a prediction given a set of attribute-value pairs
             string prediction = nbc.Predict(new Dictionary<Attrib, string>()
             {
                 { distance, "far" },
                 { speed, "slow" }
             });
 
+            // Show prediction
             Console.WriteLine($"Brake? {prediction}");
         }
     }
