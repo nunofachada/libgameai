7e9d1d0eb9480325a42891091bd87d2aa2a033db
Nuno Fachada <faken@fakenmc.com>
2021-05-20 16:55:04 +0100

    PRNG: Fix LCG, add RANDU, remove Halton

--- lib/PRNG/Randu.cs
+++ lib/PRNG/Randu.cs
@@ -2,50 +2,44 @@ using System;
 
 namespace LibGameAI.PRNG
 {
-    public class LCG : Random
+    public class Randu : Random
     {
-        private long state;
+        private ulong state;
 
-        private const long multiplier = 0x5DEECE66DL;
-        private const long addend = 0xBL;
-        private const long mask = (1L << 48) - 1;
+        private const ulong A = 65539;
+        private const ulong M = 0x7FFFFFFF; // 2^31
 
-        private const double DOUBLE_UNIT = 1.0 / (1L << 53);
-
-
-        public LCG() : this(Environment.TickCount)
+        public Randu() : this(Environment.TickCount)
         {
         }
 
-        public LCG(int seed) : this((long)seed << 32 | (long)seed)
+        public Randu(int seed)
         {
-
-        }
-        public LCG(long seed)
-        {
-            state = (seed ^ multiplier) & mask;
+            // RANDU doesn't work with seed == 0
+            if (seed == 0) seed = int.MaxValue / 2;
+            state = (ulong)seed;
         }
 
         protected override double Sample()
         {
-            return (((long)(InternalSample(26)) << 27) + InternalSample(27)) * DOUBLE_UNIT;
+            return InternalSample() * (1.0 / int.MaxValue);
         }
 
-        private int InternalSample(int bits)
+        private int InternalSample()
         {
-            state = (state * multiplier + addend) & mask;
-            return (int)(state >> (48 - bits));
+            state = (A * state) % M;
+            return (int)(state & M);
         }
 
         public override int Next()
         {
-            return InternalSample(31);
+            return InternalSample();
         }
 
         private double GetSampleForLargeRange()
         {
-            int result = InternalSample(31);
-            bool negative = (InternalSample(31) % 2 == 0) ? true : false;
+            int result = InternalSample();
+            bool negative = InternalSample() % 2 == 0;
             if (negative)
             {
                 result = -result;
@@ -55,7 +49,6 @@ namespace LibGameAI.PRNG
             d /= 2 * (uint)int.MaxValue - 1;
             return d;
         }
-
 
         public override int Next(int minValue, int maxValue)
         {
@@ -69,14 +62,13 @@ namespace LibGameAI.PRNG
 
             if (range <= (long)int.MaxValue)
             {
-                return ((int)(Sample() * range) + minValue);
+                return (int)(Sample() * range) + minValue;
             }
             else
             {
                 return (int)((long)(GetSampleForLargeRange() * range) + minValue);
             }
         }
-
 
         public override void NextBytes(byte[] buffer)
         {
@@ -85,7 +77,7 @@ namespace LibGameAI.PRNG
                     $"{nameof(buffer)} cannot be null");
             for (int i = 0; i < buffer.Length; i++)
             {
-                buffer[i] = (byte)(InternalSample(31) % (byte.MaxValue + 1));
+                buffer[i] = (byte)(Next() % (byte.MaxValue + 1));
             }
         }
     }
