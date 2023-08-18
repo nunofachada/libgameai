194524fcbdbb685af1486aa926925736fbbf736b
Nuno Fachada <faken@fakenmc.com>
2023-08-18 17:24:50 +0100

    Update library to MIT license and .NET Standard 2.1

--- samples/PRNGs/Program.cs
+++ samples/PRNGs/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/* Copyright (c) 2018-2023 Nuno Fachada and contributors
+ * Distributed under the MIT License (See accompanying file LICENSE or copy
+ * at http://opensource.org/licenses/MIT) */
+
+using System;
 using LibGameAI.PRNG;
 
 namespace LibGameAI.Samples.PRNGs
